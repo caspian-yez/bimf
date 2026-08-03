ba062bc422429383f81fdfec2e911ba53f705c3c
Yaofei Zheng <zyf26256@gmail.com>
2026-08-03 22:12:39 +0800

    add andriod-messages-converter.py

---
+++ README.md
@@ -0,0 +1,33 @@
+Backup as Internet Message Format
+=================================
+
+# Introduction
+
+We have many instant messaging apps today, such as SMS, MMS, RCS, Whatsapp, Facebook Messenger, Telegram, Snapchat, WeChat, QQ, Signal, Slack, Microsoft Teams etc. But the core is the same to email so I start this project to backup messages as eml format for archival. I choose email format as it is an open standard, human machine readable.
+
+# NOTICE
+
+This project will convert the original data into email format, so the result should not be used as legal evidence as it's easy to forge.
+
+# Andriod Messages Converter
+
+As I only have Andriod 4.2 database at hand so this converter may not work with higher version of databases.
+
+Reference: https://www.synctech.com.au/sms-backup-restore/fields-in-xml-backup-files/
+
+email header to database table sms column map:
+
+|email header               |database column                                                                            |
+|---------------------------|-------------------------------------------------------------------------------------------|
+|`Application`              |defaults to `Android-Google-Messages`                                                      |
+|`Date-Milliseconds`        |`date` in milliseconds                                                                     |
+|`Date-Sent-Milliseconds`   |`date_send` in milliseconds                                                                |
+|`Protocol`                 |`protocol` in integer                                                                      |
+|`Service-Center`           |`service_center`                                                                           |
+|`Status`                   |`status` in integer and string                                                             |
+|`Type`                     |`type` in integer and string                                                               |
+|`Date`                     |`date_send` or `date` if `date_send` is 0                                                  |
+|`From`                     |`address` if `type` is 1 (Received) or the phone number if `type` is 2 (Sent), 5 (Failed)  |
+|`To`                       |the phone number if `type` is 1 (Received) or `address` if `type` is 2 (Sent), 5 (Failed)  |
+|`Subject`                  |`subject`                                                                                  |
+|message body               |`body`                                                                                     |
---
+++ andriod-messages-converter.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+import sys
+import os
+import datetime
+import zoneinfo
+import sqlite3
+import email.message
+import email.utils
+
+# CREATE TABLE sms (
+# _id INTEGER PRIMARY KEY,          /* row index 00 */
+# thread_id INTEGER,                /* row index 01 */
+# address TEXT,                     /* row index 02 */
+# m_size INTEGER,                   /* row index 03 */
+# person INTEGER,                   /* row index 04 */
+# date INTEGER,                     /* row index 05, received timestamp */
+# date_sent INTEGER DEFAULT 0,      /* row index 06, sent timestamp of sender */
+# protocol INTEGER,                 /* row index 07, Protocol used by the message, its mostly 0 in case of SMS messages. */
+# read INTEGER DEFAULT 0,           /* row index 08, Read Message = 1, Unread Message = 0. */
+# status INTEGER DEFAULT -1,        /* row index 09, None = -1, Complete = 0, Pending = 32, Failed = 64. */
+# type INTEGER,                     /* row index 10, 1 = Received, 2 = Sent, 3 = Draft, 4 = Outbox, 5 = Failed, 6 = Queued */
+# reply_path_present INTEGER,       /* row index 11 */
+# subject TEXT,                     /* row index 12 */
+# body TEXT,                        /* row index 13 */
+# service_center TEXT,              /* row index 14 */
+# locked INTEGER DEFAULT 0,         /* row index 15 */
+# sim_id INTEGER DEFAULT -1,        /* row index 16, id of the phone subscription (SIM). These are values like 0, 1, 2  etc. based on how the phone assigns the index to the sim being used. */
+# error_code INTEGER DEFAULT 0,     /* row index 17 */
+# seen INTEGER DEFAULT 0,           /* row index 18 */
+# ipmsg_id INTEGER DEFAULT 0        /* row index 19 */
+# )
+
+
+def db_type_to_str(num: int) -> str:
+    type_string: str = ""
+    match num:
+        case 1:
+            type_string = "Received"
+        case 2:
+            type_string = "Sent"
+        case 3:
+            type_string = "Draft"
+        case 4:
+            type_string = "Outbox"
+        case 5:
+            type_string = "Failed"
+        case 6:
+            type_string = "Queued"
+        case _:
+            type_string = "Unknown"
+
+    return type_string
+
+
+def db_status_to_str(num: int) -> str:
+    status_string: str = ""
+    match num:
+        case -1:
+            status_string = "None"
+        case 0:
+            status_string = "Complete"
+        case 32:
+            status_string = "Pending"
+        case 64:
+            status_string = "Failed"
+        case _:
+            status_string = "Unknown"
+
+    return status_string
+
+
+def main():
+    if 3 != len(sys.argv):
+        print("need parameters: <database filepath> <phone number>")
+        return
+
+    print(sys.argv[0])  # program name
+    print(sys.argv[1])  # database file path
+    print(sys.argv[2])  # phone number
+
+    # 1. Connect to the database file
+    conn = sqlite3.connect(sys.argv[1])
+
+    # 2. Create a cursor object
+    cursor = conn.cursor()
+
+    # 3. Execute the SQL query
+    cursor.execute("SELECT * FROM sms")
+
+    # 4. Fetch the data
+    rows = cursor.fetchall()
+
+    la_tz = zoneinfo.ZoneInfo("Asia/Shanghai")
+
+    # 5. Loop through and print the results
+    for row in rows:
+        msg = email.message.EmailMessage()
+
+        msg["Application"] = "Android-Google-Messages"
+
+        msg["Date-Milliseconds"] = str(row[5])
+
+        msg["Date-Sent-Milliseconds"] = str(row[6])
+
+        msg["ID"] = str(row[0])
+
+        if row[7]:
+            msg["Protocol"] = str(row[7])
+        else:
+            msg["Protocol"] = ""
+
+        if row[14]:
+            msg["Service-Center"] = row[14]
+        else:
+            msg["Service-Center"] = ""
+
+        if row[9]:
+            status_string = db_status_to_str(row[9])
+            if "Unknown" == status_string:
+                print("Unknow status: {}".format(row[9]))
+                print(row)
+                break
+            else:
+                msg["Status"] = str(row[9]) + "; " + status_string
+        else:
+            msg["Status"] = ""
+
+        if row[10]:
+            type_string = db_type_to_str(row[10])
+            if "Unknown" == type_string:
+                print("Unknow type: {}".format(row[10]))
+                print(row)
+                break
+            else:
+                msg["Type"] = str(row[10]) + "; " + type_string
+        else:
+            msg["Type"] = ""
+
+        # common email headers
+        if 0 == row[6]:
+            local_datetime = datetime.datetime.fromtimestamp(row[5] / 1000, la_tz)
+        else:
+            local_datetime = datetime.datetime.fromtimestamp(row[6] / 1000, la_tz)
+
+        msg["Date"] = email.utils.format_datetime(local_datetime)
+
+        match row[10]:
+            case 1:
+                msg["From"] = row[2]
+                msg["To"] = sys.argv[2]
+            case 2 | 5:
+                msg["From"] = sys.argv[2]
+                msg["To"] = row[2]
+            case _:
+                print("Unknow type: {}".format(row[10]))
+                print(row)
+                break
+
+        if row[12]:
+            msg["Subject"] = row[12]
+        else:
+            msg["Subject"] = ""
+
+        if row[13]:
+            msg.set_content(row[13])
+
+        filename = str(row[0]) + ".eml"
+
+        if os.path.exists(filename):
+            print("filename {} exists".format(filename))
+        else:
+            fp = open(filename, "w")
+            fp.write(msg.as_string())
+            fp.close()
+
+    # 6. Close the connection
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
